b9303cb8dff855649d98d1722eb1889fcd9a6472
My Lord <page.c.tyler@gmail.com>
2021-03-12 15:45:07 -0500

    update structure and a few other methods are implemented.

--- src/main/scala/commands/Mkdir.scala
+++ src/main/scala/commands/Mkdir.scala
@@ -1,7 +1,7 @@
 package main.scala.commands
 
 import commands.Command
-import files.Directory
+import files.{DirEntry, Directory}
 import filesystem.State
 
 class Mkdir(name: String) extends Command {
@@ -24,6 +24,27 @@ class Mkdir(name: String) extends Command {
   }
 
   def doMkdir(state: State, name: String): State = {
+    def updateStructure(currentDirectory: Directory, path: List[String], newEntry: DirEntry): Directory = {
+      if (path.isEmpty) currentDirectory.addEntry(newEntry)
+      else {
+        val oldEntry = currentDirectory.findEntry(path.head).asDirectory
+        currentDirectory.replaceEntry(oldEntry.name, updateStructure(oldEntry, path.tail, newEntry))
+      }
+    }
+    val wd = state.wd
 
+    // all dir in the full path.
+    val allDirsInPath = wd.getAllFoldersInPath
+
+    // create a new directory entry in wd.
+    val newDir = Directory.empty(wd.path, name)
+
+    // update the whole dir structure starting from the root.
+    val newRoot = updateStructure(state.root, allDirsInPath, newDir)
+
+    // get new working dir instance given wd's full path in the new dir structure (immutable).
+    val newWd = newRoot.findDescendant(allDirsInPath)
+
+    State(newRoot, newWd)
   }
 }
