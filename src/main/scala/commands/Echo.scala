6c6c9ab5a9088a0a7b96197d4e53c62b4a03d752
My Lord <page.c.tyler@gmail.com>
2021-03-16 14:23:57 -0400

    echo command implemented.

--- src/main/scala/commands/Echo.scala
+++ src/main/scala/commands/Echo.scala
@@ -1,7 +1,9 @@
 package main.scala.commands
 
 import commands.Command
+import files.Directory
 import filesystem.State
+import main.scala.files.File
 
 import scala.annotation.tailrec
 
@@ -24,7 +26,34 @@ class Echo(args: Array[String]) extends Command {
     }
   }
 
-  def doEcho(state: State, content: String, fileName: String, append: Boolean): State = ???
+  def getRootAfterEcho(currentDirectory: Directory, path: List[String], contents: String, append: Boolean): Directory = {
+
+    if (path.isEmpty) currentDirectory
+    else if (path.tail.isEmpty) {
+      val dirEntry = currentDirectory.findEntry(path.head)
+      if (dirEntry == null) currentDirectory.addEntry(new File(currentDirectory.path, path.head, contents))
+      else if (dirEntry.isDirectory) currentDirectory
+      else if (append) currentDirectory.replaceEntry(path.head, dirEntry.asFile.appendContents(contents))
+      else currentDirectory.replaceEntry(path.head, dirEntry.asFile.setContents(contents))
+    } else {
+        val nextDirectory = currentDirectory.findEntry(path.head).asDirectory
+        val newNextDirectory = getRootAfterEcho(nextDirectory, path.tail, contents, append)
+        if (newNextDirectory == nextDirectory) currentDirectory
+        else currentDirectory.replaceEntry(path.head, newNextDirectory)
+      }
+    }
+
+  def doEcho(state: State, contents: String, fileName: String, append: Boolean): State = {
+    if (fileName.contains(Directory.SEPARATOR))
+      state.setMessage("Echo: filename must not contain separators.")
+    else {
+      val newRoot: Directory = getRootAfterEcho(state.root, state.wd.getAllFoldersInPath :+ fileName, contents, append)
+      if (newRoot == state.root)
+        state.setMessage(fileName + ": no such file")
+      else
+        State(newRoot, newRoot.findDescendant(state.wd.getAllFoldersInPath))
+    }
+  }
 
   def createContent(args: Array[String], topIndex: Int): String = {
     @tailrec
