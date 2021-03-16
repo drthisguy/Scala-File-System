9dcd322c988cac3beaf93a74a7c9e036348ceac7
My Lord <page.c.tyler@gmail.com>
2021-03-16 16:50:12 -0400

    refactored filesystem main to have more functional and less stateful code.

--- src/main/scala/filesystem/Filesystem.scala
+++ src/main/scala/filesystem/Filesystem.scala
@@ -1,19 +1,23 @@
 package filesystem
 
-import java.util.Scanner
-
 import commands.Command
 import files.Directory
 
 object Filesystem extends App {
-
   val root = Directory.ROOT
-  var state = State(root, root)
-  val scanner = new Scanner(System.in)
 
-  while (true) {
-    state.show
-    val input = scanner.nextLine()
-    state = Command.From(input).apply(state)
-  }
+  //This method removes the stateful way of dealing with things commented out below.
+  io.Source.stdin.getLines().foldLeft(State(root, root))((currentState, newLine) => {
+    currentState.show
+    Command.from(newLine).apply(currentState)
+  })
+
+//  var state = State(root, root)
+//  val scanner = new Scanner(System.in)
+//
+//  while (true) {
+//    state.show
+//    val input = scanner.nextLine()
+//    state = Command.from(input).apply(state)
+//  }
 }
